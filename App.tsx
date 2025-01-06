47930cf9ca4c76cce1fb607730619cfe0078a625
wdthor <wdthor@proton.me>
2025-01-06 15:41:52 +0100

    Add colors and Prettier config

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
+import { Colors } from '@/utils/Colors';
 import { StyleSheet, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Focus App</Text>
+      <Text style={styles.text}>Focus App</Text>
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: Colors.tinBlue
   },
+  text: {
+    color: Colors.white
+  }
 });
---
+++ src/utils/Colors.ts
@@ -0,0 +1,4 @@
+export const Colors = {
+  white: '#fff',
+  tinBlue: '#2f95dc',
+};
